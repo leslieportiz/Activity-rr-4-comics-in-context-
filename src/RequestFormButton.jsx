56f27d2d1e78cdae971299965691bbde072611df
lorraine Ortiz <114970662+leslieportiz@users.noreply.github.com>
2023-03-14 04:05:14 +0000

    activity 4. useContext

--- src/RequestFormButton.jsx
+++ src/RequestFormButton.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
-
+import StylesContext from "./StylesContext";
 
 export default function RequestFormButton() {
+  const buttonStyling = React.useContext(StylesContext)
+
   return (
     <div>
       <button>
---
+++ src/StyleContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const StylesContext = React.createContext({
+  
+  });
+
+export default StylesContext;
+                    
